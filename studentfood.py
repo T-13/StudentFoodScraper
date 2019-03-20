ea13313697f15a6cef738d2511451b93178ac1e9
jonpas <jonpas33@gmail.com>
2019-03-20 21:40:47 +0100

    Add JSON export, Always initialize class instance variables

--- studentfood.py
+++ studentfood.py
@@ -2,6 +2,7 @@
 
 import sys
 import argparse
+import json
 
 import requests
 from bs4 import BeautifulSoup
@@ -14,20 +15,39 @@ RESTAURANT_EXT = "sl/restaurant"
 
 
 class Restaurant():
-    name = ""
-    longitude = 0.0
-    latitude = 0.0
-    address = ""
-    price_student = 0.0
-    price_full = 0.0
-    city = ""
-    meals = []
+    def __init__(self):
+        self.name = ""
+        self.longitude = 0.0
+        self.latitude = 0.0
+        self.address = ""
+        self.price_student = 0.0
+        self.price_full = 0.0
+        self.city = ""
+        self.meals = []
 
-    vegi = False
-    weekends = False
-    for_disabled = False
-    delivery = False
-    lunch = False
+        self.vegi = False
+        self.weekends = False
+        self.for_disabled = False
+        self.delivery = False
+        self.lunch = False
+
+    def __repr__(self):
+        text = "{} {}".format(colored(self.name, attrs=["bold", "underline"]),
+                              colored("({})".format(self.city), attrs=["dark"]))
+
+        if self.vegi:         text += colored(" [VEGGIE]", "yellow")
+        if self.weekends:     text += colored(" [WEEKEND]", "yellow")
+        if self.for_disabled: text += colored(" [WHEELCHAIR]", "yellow")
+        if self.delivery:     text += colored(" [DELIVERY]", "yellow")
+        if self.lunch:        text += colored(" [LUNCH]", "yellow")
+
+        text += " => {} {}".format(colored("{}€".format(self.price_student), attrs=["bold"]),
+                                   colored("({}€)".format(self.price_full), attrs=["dark"]))
+
+        for meal in self.meals:
+            text += "\n- {}".format(meal)
+
+        return text
 
     @classmethod
     def from_html(cls, element):
@@ -65,32 +85,15 @@ class Restaurant():
 
         return restaurant
 
-    def __repr__(self):
-        text = "{} {}".format(colored(self.name, attrs=["bold", "underline"]),
-                              colored("({})".format(self.city), attrs=["dark"]))
-
-        if self.vegi:         text += colored(" [VEGGIE]", "yellow")
-        if self.weekends:     text += colored(" [WEEKEND]", "yellow")
-        if self.for_disabled: text += colored(" [WHEELCHAIR]", "yellow")
-        if self.delivery:     text += colored(" [DELIVERY]", "yellow")
-        if self.lunch:        text += colored(" [LUNCH]", "yellow")
-
-        text += " => {} {}".format(colored("{}€".format(self.price_student), attrs=["bold"]),
-                                   colored("({}€)".format(self.price_full), attrs=["dark"]))
-
-        for meal in self.meals:
-            text += "\n- {}".format(meal)
-
-        return text
-
 
 class Meal():
-    main_meal = ""
-    extras = ""
+    def __init__(self):
+        self.main_meal = ""
+        self.extras = []
 
     def __repr__(self):
         return "{} {}".format(self.main_meal,
-                              colored("({})".format(self.extras), attrs=["dark"]))
+                              colored("({})".format(", ".join(self.extras)), attrs=["dark"]))
 
     @classmethod
     def from_html(cls, element):
@@ -106,9 +109,7 @@ class Meal():
             extra_food = extra.find_all("i")[0].get_text()
             if extra_food:
                 if len(extra_food) > 0 and "&nbsp" not in extra_food:
-                    if meal.extras != "":
-                        meal.extras += ", "
-                    meal.extras += extra_food.strip()
+                    meal.extras.append(extra_food.strip())
 
         return meal
 
@@ -129,80 +130,85 @@ def main():
     parser.add_argument("-p", "--price", type=float, default=-1.0, help="require price to be same as given value")
     parser.add_argument("--price-le", type=float, default=float("inf"), help="require price to be lower than given value")
     parser.add_argument("--price-ge", type=float, default=0.0, help="require price to be higher than given value")
+    parser.add_argument("-o", "--out", type=str, help="write to given file (json)")
 
     args = parser.parse_args();
     args.name = args.name.lower().strip()
     args.city = args.city.lower().strip()
 
     # Search
+    restaurants = []
     try:
         # Get html from resource, parse it and get all restaurant elements
         html = requests.get("{}/{}".format(BASE_URL, RESTAURANT_EXT))
         document = BeautifulSoup(html.content, 'html.parser')
         restaurants = document.find_all(class_="restaurant-row")
-
-        found = []
-
-        # Retrieve data
-        print("Searching restaurants on {}...".format(BASE_URL))
-        for element in pb.progressbar(restaurants):
-            # Parse restaurant basic data
-            restaurant = Restaurant.from_html(element)
-
-            # Conditions
-            if args.name and args.name not in restaurant.name.lower().strip(): continue
-            if args.city and args.city not in restaurant.city.lower().strip(): continue
-            if args.vegetarian and not restaurant.vegi:         continue
-            if args.weekends and not restaurant.weekends:       continue
-            if args.wheelchair and not restaurant.for_disabled: continue
-            if args.delivery and not restaurant.delivery:       continue
-            if args.lunch and not restaurant.lunch:             continue
-            if args.price >= 0.0 and args.price != restaurant.price_student: continue
-            if restaurant.price_student > args.price_le:                     continue
-            if restaurant.price_student < args.price_ge:                     continue
-
-
-            found.append(restaurant)
-
-            # Parse restaurant details - ignore if no details provided
-            if not args.meals:
-                continue
-
-            source = element["data-detailslink"]
-            if not source or len(source.strip()) <= 0:
-                continue
-
-            # Get html from resource and parse it
-            url = "{}/{}".format(BASE_URL, source)
-            html = requests.get(url)
-            document = BeautifulSoup(html.content, 'html.parser')
-
-            # Get meal elements
-            meals = document.find(id="menu-list")
-            for entry in meals.find_all(class_="shadow-wrapper"):
-                try:
-                    restaurant.meals.append(Meal.from_html(entry))
-                except Exception as e:
-                    pass  # No menu/meal defined
-
-        # Pretty print
-        if not args.meals:
-            print()
-
-        for f in found:
-            if args.meals:
-                print()
-
-            print(f)
-            if args.address:
-                print(colored("[{} ({}, {})]".format(f.address, f.longitude, f.latitude), attrs=["dark"]))
-
-
-        print("\nFound {} restaurants!".format(len(found)))
-
     except Exception as e:
         print("Failed to fetch restaurants! {}".format(e))
         return 1
+
+    found = []
+
+    # Retrieve data
+    print("Searching restaurants on {}...".format(BASE_URL))
+    for element in pb.progressbar(restaurants):
+        # Parse restaurant basic data
+        restaurant = Restaurant.from_html(element)
+
+        # Conditions
+        if args.name and args.name not in restaurant.name.lower().strip(): continue
+        if args.city and args.city not in restaurant.city.lower().strip(): continue
+        if args.vegetarian and not restaurant.vegi:         continue
+        if args.weekends and not restaurant.weekends:       continue
+        if args.wheelchair and not restaurant.for_disabled: continue
+        if args.delivery and not restaurant.delivery:       continue
+        if args.lunch and not restaurant.lunch:             continue
+        if args.price >= 0.0 and args.price != restaurant.price_student: continue
+        if restaurant.price_student > args.price_le:                     continue
+        if restaurant.price_student < args.price_ge:                     continue
+
+
+        found.append(restaurant)
+
+        # Parse restaurant details - ignore if no details provided
+        if not args.meals:
+            continue
+
+        source = element["data-detailslink"]
+        if not source or len(source.strip()) <= 0:
+            continue
+
+        # Get html from resource and parse it
+        url = "{}/{}".format(BASE_URL, source)
+        html = requests.get(url)
+        document = BeautifulSoup(html.content, 'html.parser')
+
+        # Get meal elements
+        meals = document.find(id="menu-list")
+        for entry in meals.find_all(class_="shadow-wrapper"):
+            try:
+                restaurant.meals.append(Meal.from_html(entry))
+            except Exception as e:
+                pass  # No menu/meal defined
+
+    # Pretty print
+    if not args.meals:
+        print()
+
+    for f in found:
+        if args.meals:
+            print()
+
+        print(f)
+        if args.address:
+            print(colored("[{} ({}, {})]".format(f.address, f.longitude, f.latitude), attrs=["dark"]))
+
+    print("\nFound {} restaurants!".format(len(found)))
+
+    # Write to file
+    if args.out:
+        with open(args.out, "w", newline="\n", encoding="utf-8") as f:
+            json.dump(found, f, default=lambda o: o.__dict__, indent=2)
 
     return 0
 
